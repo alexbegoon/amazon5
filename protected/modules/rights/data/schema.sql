5ab1e7da63121a26f9860e213bd05f85c16a7740
Alexander Begoon <alexbassmusic@gmail.com>
2014-05-07 17:28:26 +0400

    User Module and Rights Module configured and installed. Works well.

--- protected/modules/rights/data/schema.sql
+++ protected/modules/rights/data/schema.sql
@@ -1,9 +1,9 @@
-drop table if exists AuthItem;
-drop table if exists AuthItemChild;
-drop table if exists AuthAssignment;
-drop table if exists Rights;
+drop table if exists amzni5_authitem;
+drop table if exists amzni5_authitemchild;
+drop table if exists amzni5_authassignment;
+drop table if exists amzni5_rights;
 
-create table AuthItem
+create table amzni5_authitem
 (
    name varchar(64) not null,
    type integer not null,
@@ -13,30 +13,30 @@ create table AuthItem
    primary key (name)
 );
 
-create table AuthItemChild
+create table amzni5_authitemchild
 (
    parent varchar(64) not null,
    child varchar(64) not null,
    primary key (parent,child),
-   foreign key (parent) references AuthItem (name) on delete cascade on update cascade,
-   foreign key (child) references AuthItem (name) on delete cascade on update cascade
+   foreign key (parent) references amzni5_authitem (name) on delete cascade on update cascade,
+   foreign key (child) references amzni5_authitem (name) on delete cascade on update cascade
 );
 
-create table AuthAssignment
+create table amzni5_authassignment
 (
    itemname varchar(64) not null,
    userid varchar(64) not null,
    bizrule text,
    data text,
    primary key (itemname,userid),
-   foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
+   foreign key (itemname) references amzni5_authitem (name) on delete cascade on update cascade
 );
 
-create table Rights
+create table amzni5_rights
 (
 	itemname varchar(64) not null,
 	type integer not null,
 	weight integer not null,
 	primary key (itemname),
-	foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
+	foreign key (itemname) references amzni5_authitem (name) on delete cascade on update cascade
 );
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,9 +31,10 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Contact', 'url'=>array('/site/contact')),array('label'=>Yii::t('app','Login'), 'url'=>array('/user/login'),'visible'=>Yii::app()->user->isGuest),
+                                array('label'=>Yii::t('app','Rights'), 'url'=>array('/rights')),
+                                array('label'=>Yii::t('app','Logout').' ('.Yii::app()->user->name.')', 'url'=>array('/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                            ,
 			),
 		)); ?>
 	</div><!-- mainmenu -->
