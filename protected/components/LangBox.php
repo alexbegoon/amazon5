b939f80a56c9d2c904a64d3abf305757f8204a33
Alexander Begoon <alexbassmusic@gmail.com>
2014-06-24 18:24:29 +0400

    test

--- protected/components/LangBox.php
+++ protected/components/LangBox.php
@@ -8,13 +8,14 @@ class LangBox extends CWidget
 {
     public function run()
     {
-//        $currentLang='';
-//        
-//        if(Yii::app()->user->hasState('applicationLanguage'))
-//        {
-//            $currentLang = Yii::app()->user->getState('applicationLanguage');
-//        }
-//        
-//        $this->render('langBox', array('currentLang' => $currentLang));
+        $currentLang='';
+        
+        if(Yii::app()->user->hasState('applicationLanguage'))
+        {
+            if(Yii::app()->user->getState('applicationLanguage'))
+            $currentLang = Yii::app()->user->getState('applicationLanguage');
+        }
+        
+        $this->render('langBox', array('currentLang' => $currentLang));
     }
 }
