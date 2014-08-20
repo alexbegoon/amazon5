d0c48a2d24ca4d14bf730402c1cee629b2adbd84
Alexander Begoon <alexbassmusic@gmail.com>
2014-08-20 17:51:52 +0400

    #614 in progress

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -216,7 +216,7 @@ function file_get_contents_curl($url)
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
+//    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
 
     $data = curl_exec($ch);
     curl_close($ch);
