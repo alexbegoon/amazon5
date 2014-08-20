3a7f0335e3694ffedf463a2f5f08f0a3573f8c7e
Alexander Begoon <alexbassmusic@gmail.com>
2014-08-20 21:06:07 +0400

    #614 in progress

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -215,40 +215,17 @@ function url_slug($str, $options = array()) {
 
 function file_get_contents_curl($url) 
 {
-    // is cURL installed yet?
-    if (!function_exists('curl_init')){
-        die('Sorry cURL is not installed!');
-    }
-
-    // create a new cURL resource handle
     $ch = curl_init();
 
-    // Now set some options (most are optional)
-
-    // Set URL to download
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    // Set a referer
-    //curl_setopt($ch, CURLOPT_REFERER, "http://www.example.org/yay.htm");
-
-    // User agent
-    curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
-
-    // Include header in result? (0 = yes, 1 = no)
+    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
 
-    // Should cURL return or print out the data? (true = return, false = print)
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Timeout in seconds
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-
-    // Download the given URL, and return output
-    $output = curl_exec($ch);
-
-    // Close the cURL resource, and free system resources
+    $data = curl_exec($ch);
     curl_close($ch);
 
-    return $output;
+    return $data;
 }
 ?>
