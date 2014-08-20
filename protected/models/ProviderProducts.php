d586e023b9d2aa55271b5776cbcf5c1ac0de75a5
Alexander Begoon <alexbassmusic@gmail.com>
2014-08-20 18:10:29 +0400

    #614 in progress

--- protected/models/ProviderProducts.php
+++ protected/models/ProviderProducts.php
@@ -193,13 +193,15 @@ class ProviderProducts extends CActiveRecord
                 $serviceData = self::requestProviderData($model);
                 $products = self::processProviderData($serviceData,$model);
                 $products = self::assignToProducts($products);
-//                self::assignToManufacturers($products);
+                self::assignToManufacturers($products);
                 self::storeProducts($products);
                 
+                if ($transaction->active)
                 $transaction->commit();
-                unset($products);
+//                unset($products);
                 return true;
             } catch (Exception $ex) {
+                if ($transaction->active)
                 $transaction->rollback();
                 return false;
             }
