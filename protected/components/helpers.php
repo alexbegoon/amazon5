cfdcfc89ab3721757b0281355ab10a24f1490911
Alexander Begoon <alexbassmusic@gmail.com>
2014-08-20 19:56:41 +0400

    #614 in progress

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -223,7 +223,7 @@ function file_get_contents_curl($url)
     curl_setopt($ch, CURLOPT_URL, $url);
 //    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
 
-    $data = curl_exec_follow($ch);
+    $data = curl_exec($ch);
     curl_close($ch);
 
     return $data;
