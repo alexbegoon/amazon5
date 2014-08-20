e6c6b4174d1f4dcfafc5f686fea3bec622d9a717
Alexander Begoon <alexbassmusic@gmail.com>
2014-08-20 21:20:02 +0400

    #614 in progress

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -215,7 +215,7 @@ function url_slug($str, $options = array()) {
 
 function file_get_contents_curl($url) 
 {
-    if(preg_match('#var/www/vhosts/amazoni5.amazonibuyin.com/httpdocs/uploads/#', $url) === 1)
+    if(preg_match('#'.Yii::app()->params['uploadsPath'].'#', $url) === 1)
     {
         return file_get_contents($url);
     }
