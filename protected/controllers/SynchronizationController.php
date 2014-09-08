d9db04587ad5661aa1e6e6853cd2524bbf0219f3
Alexander Begoon <alexbassmusic@gmail.com>
2014-09-08 18:13:03 +0400

    Import translations from old version

--- protected/controllers/SynchronizationController.php
+++ protected/controllers/SynchronizationController.php
@@ -37,4 +37,61 @@ class SynchronizationController extends CController
             
             $this->render('index',array('items'=>$items));
 	}
+        
+        public function actionImportTranslations()
+        {
+            ini_set ('memory_limit', "1024M");
+            ini_set('max_execution_time', 1000);
+            $translationsCSV = Yii::app()->params['uploadsPath'].'translations.csv';
+            
+            $data = file_get_contents($translationsCSV);
+            
+            $rows = explode("\n", $data);
+            
+            try
+            {
+                $currentTransaction = Yii::app()->db->getCurrentTransaction();
+                if ($currentTransaction !== null) 
+                {
+                    // Transaction already started outside
+                    $currentTransaction = null;
+                }
+                $transaction = Yii::app()->db->beginTransaction();
+                
+                foreach ($rows as $row)
+                {
+                    $row_data = str_getcsv($row);   
+                    
+                    if(!isset($row_data[1]))
+                        continue;
+                    
+                    $product = Products::findBySKU($row_data[1]);
+                    
+                    if($product===null)
+                        continue;
+                    
+//                    var_dump($row_data);die;
+                    
+                    $productTranslation = new ProductTranslations;
+                    
+                    $productTranslation->product_id = $product->id;
+                    $productTranslation->language_code = isset($row_data[2])?$row_data[2]:NULL;
+                    $productTranslation->product_name = isset($row_data[3])?$row_data[3]:NULL;
+                    $productTranslation->product_desc = isset($row_data[4])?$row_data[4]:NULL;
+                    $productTranslation->product_s_desc = isset($row_data[5])?$row_data[5]:NULL;
+                    $productTranslation->meta_desc = isset($row_data[6])?$row_data[6]:NULL;
+                    $productTranslation->meta_keywords = isset($row_data[7])?$row_data[7]:NULL;
+                    $productTranslation->custom_title = isset($row_data[8])?$row_data[8]:NULL;
+                    
+                    $productTranslation->save();
+                }
+                
+                $transaction->commit();
+            } catch (Exception $ex) {
+                $transaction->rollback();
+                throw new CHttpException(500,$ex->getMessage());
+            }
+            
+            
+        }
 }
