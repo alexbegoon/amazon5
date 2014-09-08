32a9efa4303f7aa5b9a6e2f67305bf60db537055
Alexander Begoon <alexbassmusic@gmail.com>
2014-09-08 18:54:56 +0400

    DISABLE!!
    Import translations from old version

--- protected/controllers/SynchronizationController.php
+++ protected/controllers/SynchronizationController.php
@@ -38,81 +38,81 @@ class SynchronizationController extends CController
             $this->render('index',array('items'=>$items));
 	}
         
-//        public function actionImportTranslations()
-//        {
-//            ini_set ('memory_limit', "1024M");
-//            ini_set('max_execution_time', 1000);
-//            $translationsCSV = Yii::app()->params['uploadsPath'].'translations.csv';
-//            
-//            $data = file_get_contents($translationsCSV);
-//            
-//            $rows = explode("\n", $data);
-//            
-//            try
-//            {
-//                $currentTransaction = Yii::app()->db->getCurrentTransaction();
-//                if ($currentTransaction !== null) 
-//                {
-//                    // Transaction already started outside
-//                    $currentTransaction = null;
-//                }
-//                $transaction = Yii::app()->db->beginTransaction();
-//                
-//                
-//                $i = 0;
-//                foreach ($rows as $row)
-//                {
-//                    $row_data = str_getcsv($row);   
-//                    
-//                    if(!isset($row_data[1]))
-//                        continue;
-//                    
-//                    $product = Products::findBySKU($row_data[1]);
-//                    
-//                    if($product===null)
-//                        continue;
-//                    
-////                    var_dump($row_data);die;
-//                    
-//                    $productTranslation = new ProductTranslations;
-//                    
-//                    $productTranslation->setAttribute('product_id', $product->id);
-//                    if(isset($row_data[2]))
-//                    $productTranslation->setAttribute('language_code',isset($row_data[2])?$row_data[2]:NULL);
-//                    if(isset($row_data[3]))
-//                    $productTranslation->setAttribute('product_name',isset($row_data[3])?$row_data[3]:NULL);
-//                    if(isset($row_data[4]))
-//                    $productTranslation->setAttribute('product_desc',isset($row_data[4])?$row_data[4]:NULL);
-//                    if(isset($row_data[5]))
-//                    $productTranslation->setAttribute('product_s_desc',isset($row_data[5])?$row_data[5]:NULL);
-//                    if(isset($row_data[6]))
-//                    $productTranslation->setAttribute('meta_desc',isset($row_data[6])?$row_data[6]:NULL);
-//                    if(isset($row_data[7]))
-//                    $productTranslation->setAttribute('meta_keywords',isset($row_data[7])?$row_data[7]:NULL);
-//                    if(isset($row_data[8]))
-//                    $productTranslation->setAttribute('custom_title',isset($row_data[8])?$row_data[8]:NULL);
-//                    
-//                    if(!$productTranslation->save())
-//                    {
-////                        throw new Exception(get_validation_errors($productTranslation));
-//                    }
-//                    
-//                    if($i>100)
-//                    {
-//                        $transaction->commit();
-//                        $transaction = Yii::app()->db->beginTransaction();
-//                        $i=0;
-//                    }
-//                    
-//                    $i++;
-//                }
-//                
-//                $transaction->commit();
-//            } catch (Exception $ex) {
-//                $transaction->rollback();
-//                throw new CHttpException(500,$ex->getMessage());
-//            }
-//            
-//            
-//        }
+        public function actionImportTranslations()
+        {
+            ini_set ('memory_limit', "1024M");
+            ini_set('max_execution_time', 1000);
+            $translationsCSV = Yii::app()->params['uploadsPath'].'translations.csv';
+            
+            $data = file_get_contents($translationsCSV);
+            
+            $rows = explode("\n", $data);
+            
+            try
+            {
+                $currentTransaction = Yii::app()->db->getCurrentTransaction();
+                if ($currentTransaction !== null) 
+                {
+                    // Transaction already started outside
+                    $currentTransaction = null;
+                }
+                $transaction = Yii::app()->db->beginTransaction();
+                
+                
+                $i = 0;
+                foreach ($rows as $row)
+                {
+                    $row_data = str_getcsv($row);   
+                    
+                    if(!isset($row_data[1]))
+                        continue;
+                    
+                    $product = Products::findBySKU($row_data[1]);
+                    
+                    if($product===null)
+                        continue;
+                    
+//                    var_dump($row_data);die;
+                    
+                    $productTranslation = new ProductTranslations;
+                    
+                    $productTranslation->setAttribute('product_id', $product->id);
+                    if(isset($row_data[2]) && $row_data[2] != 'NULL' && $row_data[2] != 'null')
+                    $productTranslation->setAttribute('language_code',isset($row_data[2])?$row_data[2]:NULL);
+                    if(isset($row_data[3]) && $row_data[3] != 'NULL' && $row_data[3] != 'null')
+                    $productTranslation->setAttribute('product_name',isset($row_data[3])?$row_data[3]:NULL);
+                    if(isset($row_data[4]) && $row_data[4] != 'NULL' && $row_data[4] != 'null')
+                    $productTranslation->setAttribute('product_desc',isset($row_data[4])?$row_data[4]:NULL);
+                    if(isset($row_data[5]) && $row_data[5] != 'NULL' && $row_data[5] != 'null')
+                    $productTranslation->setAttribute('product_s_desc',isset($row_data[5])?$row_data[5]:NULL);
+                    if(isset($row_data[6]) && $row_data[6] != 'NULL' && $row_data[6] != 'null')
+                    $productTranslation->setAttribute('meta_desc',isset($row_data[6])?$row_data[6]:NULL);
+                    if(isset($row_data[7]) && $row_data[7] != 'NULL' && $row_data[7] != 'null')
+                    $productTranslation->setAttribute('meta_keywords',isset($row_data[7])?$row_data[7]:NULL);
+                    if(isset($row_data[8]) && $row_data[8] != 'NULL' && $row_data[8] != 'null')
+                    $productTranslation->setAttribute('custom_title',isset($row_data[8])?$row_data[8]:NULL);
+                    
+                    if(!$productTranslation->save())
+                    {
+//                        throw new Exception(get_validation_errors($productTranslation));
+                    }
+                    
+                    if($i>100)
+                    {
+                        $transaction->commit();
+                        $transaction = Yii::app()->db->beginTransaction();
+                        $i=0;
+                    }
+                    
+                    $i++;
+                }
+                
+                $transaction->commit();
+            } catch (Exception $ex) {
+                $transaction->rollback();
+                throw new CHttpException(500,$ex->getMessage());
+            }
+            
+            
+        }
 }
