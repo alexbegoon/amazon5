17088fd016e190cef5b552062f5d4f9e7438c34b
Alexander Begoon <alexbassmusic@gmail.com>
2014-09-12 18:34:50 +0400

    States small changes in the views

--- protected/views/states/admin.php
+++ protected/views/states/admin.php
@@ -12,33 +12,10 @@ $this->menu=array(
 	array('label'=>Yii::t('common','Create') .' '. Yii::t('common','States'), 'url'=>array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#states-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
 
 <h1 class="text-center"><?php echo Yii::t('common','Manage');?> <?php echo Yii::t('common','States')?></h1>
 
-<p>
-    <?php echo Yii::t('common','You may optionally enter a comparison operator');?>    (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b> or <b>=</b>) 
-    <?php echo Yii::t('common','at the beginning of each of your search values to specify how the comparison should be done.');?>    
-</p>
-
-<?php echo CHtml::link(Yii::t('common','Advanced Search'),'#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'states-grid',
@@ -47,7 +24,11 @@ $('.search-form form').submit(function(){
 	'columns'=>array(
 		'id',
                 'state_name',
-		'country_code',
+                array(
+                    'name'=>'country_code',
+                    'value'=>  'Countries::listData($data->country_code)',
+                    'filter'=>  Countries::listData()
+                ),
 		'state_3_code',
 		'state_2_code',
                 array(
@@ -65,11 +46,6 @@ $('.search-form form').submit(function(){
                     'value'=>'Yii::app()->getModule("user")->user($data->modified_by)->getFullName()',
                 ),
 		'modified_on',
-		array(
-                    'name'=>'locked_by',
-                    'type'=>'html',
-                    'value'=>'$data->locked_by!=0?"<span class=\"glyphicon glyphicon-lock\" title=\"".Yii::t(\'common\',\'Locked By\')." ". Yii::app()->getModule(\'user\')->user($data->locked_by)->getFullName()."\"></span>":""',                
-                ),
 		array(
 			'class'=>'CButtonColumn',
 		),
