31aec254878751086dddd3fd1dc910500ca048d9
Alexander Begoon <alexbassmusic@gmail.com>
2014-10-02 17:18:35 +0400

    #727

--- protected/models/CategoryImages.php
+++ protected/models/CategoryImages.php
@@ -286,7 +286,7 @@ class CategoryImages extends CActiveRecord
                 {
                     $_FILES[__CLASS__]['name']['image'] = basename($this->image_url);
                     $_FILES[__CLASS__]['type']['image'] = getMimeType($_FILES[__CLASS__]['name']['image']);
-                    $filename = tempnam(sys_get_temp_dir(),'img');
+                    $filename = tempnam(sys_get_temp_dir(),__CLASS__);
                     $handle = fopen($filename,'w');
                     $_FILES[__CLASS__]['size']['image'] = fwrite($handle, file_get_contents($this->image_url));
                     $_FILES[__CLASS__]['tmp_name']['image'] = $filename;
