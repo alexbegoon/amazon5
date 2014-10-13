38af7eed922538e4836224ddeeb3138f5a1be119
Alexander Begoon <alexbassmusic@gmail.com>
2014-10-13 22:29:00 +0400

    #720

--- protected/models/Cart.php
+++ protected/models/Cart.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This class introduce the shopping cart.
+ * This class represents the shopping cart.
  *
  * @author Alexander.B <alexbassmusic@gmail.com> - https://www.odesk.com/users/~01ae8f6e1a81c189cf
  */
@@ -133,7 +133,7 @@ class Cart extends CModel
         
         $id=1;
         if(!empty($this->order_items))
-        $id = max(array_keys($this->order_items)) + rand(2, 15);
+        $id = max(array_keys($this->order_items)) + 1;
         $item->temp_id = $id;
         $this->order_items[$id] = $item;
         return $this->save();
@@ -146,6 +146,10 @@ class Cart extends CModel
         return $this->save();
     }
     
+    /**
+     * Return order items.
+     * @return \CArrayDataProvider object
+     */
     public function getOrderItems()
     {
         return new CArrayDataProvider($this->order_items, array(
@@ -160,6 +164,17 @@ class Cart extends CModel
                 'pageSize'=>20,
             ),
         ));
+    }
+    
+    /**
+     * Return order items.
+     * Useful in the cases when you want to store cart to DB.
+     * You can also simply call $cart->order_items, this is the same.
+     * @return array Array of OrderItems models
+     */
+    public function getOrderItemsArr()
+    {
+        return $this->order_items;
     }
     
     private function calculateTotal()
@@ -183,6 +198,12 @@ class Cart extends CModel
         $this->calculateSubTotal();
         $this->calculateTax();
         return parent::beforeValidate();
+    }
+    
+    public function remove()
+    {
+        if( Yii::app()->user->hasState('cart') )
+            Yii::app()->user->setState('cart',null);
     }
     
 }//end class Cart
--- protected/models/Shop.php
+++ protected/models/Shop.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This class introduce the Web Shop.
+ * This class represents the Web Shop.
  *
  * @author Alexander.B <alexbassmusic@gmail.com> - https://www.odesk.com/users/~01ae8f6e1a81c189cf
  */
